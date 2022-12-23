0be88ea680771045e7205ade895eebb95971e520
erickeira <erick.almeida.ricardo@gmail.com>
2022-12-23 16:56:04 -0400

    Texte Config

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(props) {
-  const { data } = props
-  console.log(data)
+  // const { result } = props
+
   return (
     <div style={{marginLeft: 50, marginTop: 50}}>
-      {data.resultados[0].id}
+      {/* {result} */}
+      oi
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
   const hostName = context.req.headers.host
-  const res = await fetch(`https://api-dev.infoimoveis.com.br/imoveis/`)
-  const data = await res.json()
+  // const res = await fetch(`https://api-dev.infoimoveis.com.br/imoveis/`)
+  // const data = await res.json()
   const result = "Funciona"
 
   return {
-    props: { data },
+    props: { result },
   }
 }
