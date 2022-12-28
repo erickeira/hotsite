92ecbd15e0a48f8c5c570e2329475a8dafdd6d5b
erickeira <erick.almeida.ricardo@gmail.com>
2022-12-28 16:05:50 -0400

    teste imagem

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Image from 'next/image'
 export default function Home(props) {
   const { data } = props
   if(!data) return null
+  const myLoader = ({ src, width, quality }) => {
+    return `https://static-dev.infoimoveis.com.br/${src}`
+  }
   return (
     <div style={{display: 'flex', flexDirection: 'column',marginLeft: 50, marginTop: 50}}>
       <div style={{}}>
@@ -13,7 +16,8 @@ export default function Home(props) {
         {data.resultados[0].titulo}
       </span>
       <Image
-        src={`https://static-dev.infoimoveis.com.br/${data.resultados[0].imagem}`}
+        loader={myLoader}
+        src={data.resultados[0].imagem}
         alt="Picture of the author"
         width={500}
         height={500}
