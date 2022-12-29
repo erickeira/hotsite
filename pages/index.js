f70abc61dd1505d589f62dbb30f6089ec7a5ba57
erickeira <erick.almeida.ricardo@gmail.com>
2022-12-29 13:03:22 -0400

    content header main

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { AuthContext } from "../context"
 import { apiUrl, apiId, urlImgs, moneyFormatter, cloudflareLoader, titleSite, urlFavicon, descriptionDefault, urlSite } from '../utils';
 import Link from 'next/link';
 import Place from '../public/img/place.svg';
+import ContentHeader from '../components/ContentHeaderMain';
 
 export default function Home(props) {
   const { destaques , ultimasnoticias } = props
@@ -54,7 +55,7 @@ export default function Home(props) {
 
       <div className={`${isOpen ? 'open ': ''}main`}>
                       
-          {/* <ContentHeader />  */}
+          <ContentHeader/>
 
           <div className="container py-4 px-4 px-sm-0">
           <div  className="d-none d-md-block  ">
